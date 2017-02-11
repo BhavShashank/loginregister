dd9f6fe23431c442f2a6b26517ab46b70d90b554
Bhav <BhavShashank@users.noreply.github.com>
2017-02-11 19:29:15 +0530

    Create menu.php

---
+++ includes/menu.php
@@ -0,0 +1,9 @@
+<nav>
+	<ul>
+		<li><a href="index.php">Home</a></li>
+		<li><a href="forum.php">Forum</a></li>
+		<li><a href="members.php">Members</a></li>
+		<li><a href="download.php">Downloads</a></li>
+		<li><a href="contact.php">Contact Us</a></li>
+	</ul>
+</nav>
